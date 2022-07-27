3ffa0fc323778046ec35e0abeda193851e9740e8
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-27 23:31:01 +0200

    definisanje novih varijabli

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is fun!");
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
