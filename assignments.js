4b5b9414f78904e5a5838eb053381e8088096f3d
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-28 13:07:31 +0200

    resenje trece vezbe let, const i var

--- assignments.js
+++ assignments.js
@@ -1,19 +1,17 @@
-/*
-//prva vezba - values and variables
-let country = "Serbia";
-let continent = "Europe";
+const country = "Serbia";
+const continent = "Europe";
 let population = 7000000;
 
 console.log(country);
 console.log(continent);
 console.log(population);
 
-//druga vezba - data types
-let isIsland = false;
+const isIsland = false;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
-*/
+language = "serbian";
+country = "Republika Srpska";
