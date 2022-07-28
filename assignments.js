5a57e19b0b238773c611c777010b9a58698a9d7a
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-28 11:40:59 +0200

    resenje druge vezbe

--- assignments.js
+++ assignments.js
@@ -1,8 +1,17 @@
-/* prva vezba
+//prva vezba - values and variables
 let country = "Serbia";
 let continent = "Europe";
 let population = 7000000;
 
 console.log(country);
 console.log(continent);
-console.log(population);*/
+console.log(population);
+
+//druga vezba - data types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
