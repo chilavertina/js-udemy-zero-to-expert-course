b770b3e09e704835a246cd797a239cd9825d26f1
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-28 13:20:06 +0200

    upotreba ostalih operatora

--- script.js
+++ script.js
@@ -36,3 +36,6 @@ const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 = dva na kub
