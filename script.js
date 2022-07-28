ebc89c0628b488bf509a3d04ef4f2b7bcb886130
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-28 13:29:19 +0200

    upotreba assignment operatora

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ var job = "programmer";
 job = "teacher";
 console.log(job);*/
 
+//math operators
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -43,3 +44,12 @@ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 const firstName = "Jonas";
 const lastName = "Smith";
 console.log(firstName + " " + lastName);
+
+//assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
