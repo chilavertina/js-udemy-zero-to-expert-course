ddb0bcca4b8be5c10241b63b3af0527612fde86a
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-28 11:15:43 +0200

    dodavanje boolean-a

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -6,3 +7,7 @@ console.log(23);
 
 let firstName = "Bob";
 console.log(firstName);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
