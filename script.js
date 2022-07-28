479572d7f58d37dbd2240be32abdfdbfbceb4834
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-28 16:30:30 +0200

    upotreba if else statementa

--- script.js
+++ script.js
@@ -114,8 +114,22 @@ console.log(`String
 napisan
 u vise linija...`);*/
 
-const age = 19;
+const age = 15;
 
 if (age >= 18) {
   console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah can start driving license for ${yearsLeft} years.`);
 }
+
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
