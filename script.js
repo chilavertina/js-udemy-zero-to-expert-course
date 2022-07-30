eadb577c1b498344720d35f1608ce0634d32183a
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-30 20:18:09 +0200

    upotreba logical operatora

--- script.js
+++ script.js
@@ -218,3 +218,18 @@ if (favourite === 11) {
 
 if (favourite !== 23) console.log("Why not 23?");
 */
+
+const hasDriversLicense = true; //A
+const hasGoodVision = false; //B
+
+console.log(hasDriversLicense && hasGoodVision); // false
+console.log(hasDriversLicense || hasGoodVision); // true
+console.log(!hasDriversLicense); // false
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (shouldDrive) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive.");
+}
