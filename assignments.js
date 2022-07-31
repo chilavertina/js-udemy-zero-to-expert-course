6c9c1f2f16fc211eae1e53201a853a07e6e4972a
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-07-31 15:05:02 +0200

    resenja assigments-a

--- assignments.js
+++ assignments.js
@@ -1,4 +1,4 @@
-/*const country = "Serbia";
+const country = "Serbia";
 const continent = "Europe";
 let populationSerbia = 7000000;
 
@@ -23,7 +23,7 @@ const populationFinland = 6000000;
 console.log(populationSerbia > populationFinland);
 const populationAverage = 33000000;
 console.log(populationSerbia < populationAverage);
-const description =
+/*const description =
   country +
   " is in " +
   continent +
@@ -34,3 +34,51 @@ const description =
   ".";
 console.log(description);
 */
+//TEMPLATE LITERALS ASSIGNMENT
+const description = `${country} is in ${continent}, and its ${populationSerbia} people speak ${language}.`;
+console.log(description);
+
+if (populationSerbia > 33000000) {
+  console.log("Serbia's population is above average.");
+} else {
+  console.log(`Serbia's population is ${populationAverage - populationSerbia} below average.`);
+}
+
+/*let numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+if (numNeighbours === 1) {
+  console.log("Only one neighbour!");
+} else if (numNeighbours > 1) {
+  console.log("More than one neighbour!");
+} else {
+  console.log("No neighbours!");
+}*/
+
+if (language === "english" && populationAverage < 50000000 && !isIsland) {
+  console.log("You should live in Serbia");
+} else {
+  console.log("Serbia dones not meet your criteria");
+}
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
+
+console.log(`Serbia's population is ${populationSerbia > populationAverage ? "above average" : "below average"}.`);
